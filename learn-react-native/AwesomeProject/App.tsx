a32f08159f7052d2ad9689f1319963864fb075b2
JOHN KILIMA <jkbundala@gmail.com>
2025-10-20 00:42:37 +0300

    Learn React Native: Styling - Position: Absolute & Relative & zIndex

--- learn-react-native/AwesomeProject/App.tsx
+++ learn-react-native/AwesomeProject/App.tsx
@@ -12,36 +12,50 @@ function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView>
-        <View
-          style={{
-            borderWidth: 1,
-            borderTopWidth: 15,
-            borderBottomWidth: 15,
-            borderLeftWidth: 10,
-            borderRightWidth: 15,
-            borderColor: '#15099d',
-            backgroundColor: 'white',
-            borderRadius: 10,
-            margin: 10,
-            // marginTop: 5,
-            // marginLeft: 100,
-            // marginRight: 100,
-            // marginBottom: 100,
-            marginHorizontal: 50,
-            marginVertical: 50,
-            padding: 10,
-            // paddingTop: 50,
-            // paddingLeft: 100,
-            // paddingRight: 10,
-            // paddingBottom: 10,
-            paddingHorizontal: 50,
-            paddingVertical: 50,
-          }}
-        >
-          <Text>Hi, I just completed my first React Native Assignment</Text>
-        </View>
-        <View style={{ backgroundColor: 'red' }}>
-          <Text>Hello World!</Text>
+        <View style={{ position: 'relative', backgroundColor: 'green' }}>
+          <View
+            style={{
+              borderWidth: 1,
+              borderTopWidth: 15,
+              borderBottomWidth: 15,
+              borderLeftWidth: 10,
+              borderRightWidth: 15,
+              borderColor: '#15099d',
+              backgroundColor: 'white',
+              borderRadius: 10,
+              margin: 10,
+              // marginTop: 5,
+              // marginLeft: 100,
+              // marginRight: 100,
+              // marginBottom: 100,
+              marginHorizontal: 50,
+              marginVertical: 50,
+              padding: 10,
+              // paddingTop: 50,
+              // paddingLeft: 100,
+              // paddingRight: 10,
+              // paddingBottom: 10,
+              paddingHorizontal: 50,
+              paddingVertical: 50,
+              position: 'relative',
+              zIndex: 2,
+            }}
+          >
+            <Text>Hi, I just completed my first React Native Assignment</Text>
+          </View>
+          <View
+            style={{
+              backgroundColor: 'red',
+              position: 'absolute',
+              top: 10,
+              left: 5,
+              right: 10,
+              bottom: 10,
+              zIndex: 1,
+            }}
+          >
+            <Text>Hello World!</Text>
+          </View>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
